5f8cedf4fcaad1cec1bdaf9bfe27b26a970fd35d
Mendel <47512794+Avhoss@users.noreply.github.com>
2019-04-30 23:50:39 +0700

    Add files via upload

--- printmethod.js
+++ printmethod.js
@@ -18,23 +18,32 @@ export default class PickerEx extends Component {
     return (
       <Container>
         <Header style={styles.header}>
-          <Left>
-		  <Button transparent>
-              <Icon name='arrow-back' />
-            </Button>
+			<Left>
+			  <Button transparent>
+				  <Icon name='arrow-back' />
+				</Button>
 			</Left>
-          <Body>
-            <Title>PrintHub</Title>
-          </Body>
-          <Right />
-        </Header>
-		<Text>Metode Print:</Text>
-		<Content padder>
-          <Accordion dataArray={dataArray} expanded={0}/>
+			  <Body>
+				<Title>PrintHub</Title>
+			  </Body>
+			  <Right>
+				<Button transparent>
+				  <Icon name='menu' />
+				</Button>
+			</Right>
+		</Header>
+		<Text style={styles.method}>Metode Print:</Text>
+		<Content>
+          <Accordion 
+			dataArray={dataArray} 
+			expanded={0}
+			headerStyle={{ backgroundColor: "#fea048" }}
+            contentStyle={{ backgroundColor: "#ffdbc5" }}
+		  />
         </Content>
         <Content>
           <Form>
-		  <Text>Pilih Metode Print</Text>
+		  <Text style={styles.pickMethod}>Pilih Metode Print</Text>
 			<Item >
 			<Button  rounded light style={styles.buttons}>
             <Text style={{color:"#FFFFFF"}}>Inkjet</Text>
@@ -55,17 +64,22 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 20,
     textAlign: 'center',
-    backgroundColor: '#F4D03F'
+    backgroundColor: '#fea048'
 
   }, buttons: {
     margin: 10,
 	flexDirection: "row", 
 	justifyContent: "center",
-	 backgroundColor: '#F4D03F',
+	 backgroundColor: '#fea048',
 	width : 150
   }, error: {
       fontSize: 20,
       alignSelf: 'center',
       color: 'red'
-  } 
+  }, method: {
+	  fontSize: 20
+  }, pickMethod: {
+	  fontSize: 20,
+	  alignSelf: 'center'
+  }
 });
